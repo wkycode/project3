d35f2b2a0de347fea26218cfa0d68117c5a61b7d
wky <misterwky@gmail.com>
2021-07-16 10:25:18 +0800

    new prod

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,12 +1,10 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: {
-    app: path.resolve(__dirname, '../src/index.jsx'),
-    vendor: ['react-bootstrap']
+    app: path.resolve(__dirname, '../src/index.jsx')
   },
   output: {
     path: path.resolve(__dirname, '../public'),
@@ -28,31 +26,13 @@ module.exports = {
       }, {
         test: /\.s[ac]ss$/,
         use: [
-          MiniCssExtractPlugin.loader,
+          'style-loader',
           'css-loader',
           'postcss-loader',
           'sass-loader'
         ]
       }
     ]
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          name: 'styles',
-          test: /\.s[ac]ss$/,
-          chunks: 'all',
-          enforce: true
-        },
-        vendor: {
-          chunks: 'initial',
-          test: 'vendor',
-          name: 'vendor',
-          enforce: true
-        }
-      }
-    }
   },
   plugins: [
     new webpack.DefinePlugin({
@@ -61,9 +41,6 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, '../src/template.html'),
       filename: 'index.html'
-    }),
-    new MiniCssExtractPlugin({
-      filename: 'styles/styles.[hash].css'
     })
   ]
 }
